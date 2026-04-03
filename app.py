89549f5c178de4bbf91f521f83f287815be34dca
Sarans Mohapatra <onlyincomputer3@gmail.com>
2026-04-03 23:29:33 +0530

    ui improved

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import numpy as np
 from sklearn.linear_model import LinearRegression
+  
+st.set_page_config(page_title="AI Marks Predictor", page_icon="🤖", layout="centered")
 
 # Title
-st.title("📚 Smart Marks Predictor")
+st.markdown(
+    "<h1 style='text-align: center; color: #4CAF50;'>📚 AI Marks Predictor</h1>",
+    unsafe_allow_html=True
+)
 
-st.write("Enter how many hours you studied and get your predicted marks!")
-
+st.markdown(
+    "<p style='text-align: center;'>Predict your marks based on study hours 🚀</p>",
+    unsafe_allow_html=True
+)
 # Data
 hours = np.array([1,2,3,4,5]).reshape(-1,1)
 marks = np.array([40,50,60,70,80])
@@ -21,8 +28,11 @@ new_hours = st.slider("Hours studied", 0, 10, 1)
 # Predict button
 if st.button("Predict"):
     prediction = model.predict([[new_hours]])
-    st.success(f"🎯 Predicted Marks: {int(prediction[0])}")
 
+    st.markdown(
+        f"<h2 style='text-align:center; color:#FF5733;'>🎯 {int(prediction[0])} Marks</h2>",
+        unsafe_allow_html=True
+    )
 import matplotlib.pyplot as plt
 
 # Plot graph
